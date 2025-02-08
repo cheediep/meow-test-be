2ce803bcc322895b9074520eb35b1e25807e8106
Chee Diep <c.diep@hotmail.com>
2025-02-08 10:39:09 +0000

    Implement getSentenceFormattedNames

--- src/app.template-values.service.ts
+++ src/app.template-values.service.ts
@@ -3,6 +3,14 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class TemplateValuesService {
   getSentenceFormattedNames(names: string[]): string {
-    return "";
+    return names.reduce((acc, name, index) => {
+      if (index === 0) {
+        return name;
+      }
+      if (index === names.length - 1) {
+        return `${acc} and ${name}`;
+      }
+      return `${acc}, ${name}`;
+    }, "");
   }
 }
