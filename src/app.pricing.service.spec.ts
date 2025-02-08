7bcfabf906d7dc0703511cc559b1e27c187a7e8f
Chee Diep <9450433+cheediep@users.noreply.github.com>
2025-02-08 11:08:02 +0000

    Feature: pricing service (#3)
    * Add skeleton service & tests
    
    * implement getPriceByPouchSize
    
    * Implement rest of pricing functions

---
+++ src/app.pricing.service.spec.ts
@@ -0,0 +1,75 @@
+import { faker } from "@faker-js/faker";
+import { PouchPrice } from "../data";
+import { PricingService } from "./app.pricing.service";
+import { Cat } from "./types";
+
+describe("PricingService", () => {
+  let service: PricingService;
+  const cats: Cat[] = [
+    {
+      name: faker.person.firstName(),
+      subscriptionActive: true,
+      breed: faker.animal.cat(),
+      pouchSize: "B",
+    },
+    {
+      name: faker.person.firstName(),
+      subscriptionActive: true,
+      breed: faker.animal.cat(),
+      pouchSize: "B",
+    },
+    {
+      name: faker.person.firstName(),
+      subscriptionActive: true,
+      breed: faker.animal.cat(),
+      pouchSize: "F",
+    },
+    {
+      name: faker.person.firstName(),
+      subscriptionActive: false,
+      breed: faker.animal.cat(),
+      pouchSize: "D",
+    },
+  ];
+
+  beforeEach(() => {
+    service = new PricingService(cats);
+  });
+
+  describe("getTotalSubscriptionPrice", () => {
+    it("returns the total subscription price, ignoring inactive cats", () => {
+      const totalPrice = service.getTotalSubscriptionPrice();
+      expect(totalPrice).toEqual(
+        PouchPrice[cats[0].pouchSize] +
+          PouchPrice[cats[1].pouchSize] +
+          PouchPrice[cats[2].pouchSize]
+      );
+    });
+
+    it("returns 0 if there are no active cats", () => {
+      const noActiveCatsService = new PricingService([
+        { ...cats[0], subscriptionActive: false },
+      ]);
+      const totalPrice = noActiveCatsService.getTotalSubscriptionPrice();
+      expect(totalPrice).toEqual(0);
+    });
+  });
+
+  describe("getPriceByPouchSize", () => {
+    it("returns the correct price based on pouch size", () => {
+      expect(service.getPriceByPouchSize("A")).toEqual(PouchPrice.A);
+      expect(service.getPriceByPouchSize("B")).toEqual(PouchPrice.B);
+      expect(service.getPriceByPouchSize("C")).toEqual(PouchPrice.C);
+      expect(service.getPriceByPouchSize("D")).toEqual(PouchPrice.D);
+      expect(service.getPriceByPouchSize("E")).toEqual(PouchPrice.E);
+      expect(service.getPriceByPouchSize("F")).toEqual(PouchPrice.F);
+    });
+  });
+
+  describe("filterInactiveCats", () => {
+    it("returns only cats with an active subscription", () => {
+      const filteredCats = service.filterInactiveCats();
+      expect(filteredCats).toEqual([cats[0], cats[1], cats[2]]);
+    });
+  });
+});
---
+++ src/app.pricing.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@nestjs/common";
+import { PouchPrice } from "../data";
+import { Cat } from "./types";
+
+@Injectable()
+export class PricingService {
+  constructor(private readonly cats: Cat[]) {
+    this.cats = cats;
+  }
+
+  getTotalSubscriptionPrice(): number {
+    return this.filterInactiveCats().reduce(
+      (acc, cat) => acc + this.getPriceByPouchSize(cat.pouchSize),
+      0
+    );
+  }
+
+  getPriceByPouchSize(pouchSize: keyof typeof PouchPrice): number {
+    return PouchPrice[pouchSize];
+  }
+
+  filterInactiveCats(): Cat[] {
+    return this.cats.filter((cat) => cat.subscriptionActive);
+  }
+}
