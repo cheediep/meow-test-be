ff778b82540009e66bb74ab778895048c6e73a77
Chee Diep <9450433+cheediep@users.noreply.github.com>
2025-02-08 10:43:49 +0000

    Feature:  Template values service (#2)
    * Add service skeleton and tests
    
    * Update test to match requirements
    
    * Implement getSentenceFormattedNames
    
    * Add possessive formatting function

---
+++ src/app.template-values.service.spec.ts
@@ -0,0 +1,89 @@
+import { faker } from "@faker-js/faker";
+import { TemplateValuesService } from "./app.template-values.service";
+import { Cat } from "./types";
+
+describe("TemplateValuesService", () => {
+  let service: TemplateValuesService;
+  const cats: Cat[] = [
+    {
+      name: "Mia",
+      subscriptionActive: true,
+      breed: faker.animal.cat(),
+      pouchSize: "A",
+    },
+    {
+      name: "Grease",
+      subscriptionActive: true,
+      breed: faker.animal.cat(),
+      pouchSize: "B",
+    },
+    {
+      name: "Nala",
+      subscriptionActive: true,
+      breed: faker.animal.cat(),
+      pouchSize: "B",
+    },
+    {
+      name: "James",
+      subscriptionActive: false,
+      breed: faker.animal.cat(),
+      pouchSize: "C",
+    },
+  ];
+
+  beforeEach(() => {
+    service = new TemplateValuesService();
+  });
+
+  describe("getSentenceFormattedNames", () => {
+    it("returns the value correctly when there is only one name", () => {
+      const names = service.getSentenceFormattedNames([cats[0].name]);
+      expect(names).toEqual(`${cats[0].name}`);
+    });
+
+    it("returns the value correctly when there are two names", () => {
+      const names = service.getSentenceFormattedNames([
+        cats[0].name,
+        cats[1].name,
+      ]);
+      expect(names).toEqual(`${cats[0].name} and ${cats[1].name}`);
+    });
+
+    it("returns the value correctly when there are more than two names", () => {
+      const names1 = service.getSentenceFormattedNames([
+        cats[0].name,
+        cats[1].name,
+        cats[2].name,
+      ]);
+
+      expect(names1).toEqual(
+        `${cats[0].name}, ${cats[1].name} and ${cats[2].name}`
+      );
+
+      const names2 = service.getSentenceFormattedNames([
+        cats[0].name,
+        cats[1].name,
+        cats[2].name,
+        cats[3].name,
+      ]);
+
+      expect(names2).toEqual(
+        `${cats[0].name}, ${cats[1].name}, ${cats[2].name} and ${cats[3].name}`
+      );
+    });
+  });
+
+  describe("possessiveFormat", () => {
+    it("returns the value correctly when there is only one name", () => {
+      const names = service.possessiveFormat(cats[0].name);
+      expect(names).toEqual(`${cats[0].name}'s`);
+    });
+
+    it("returns the value correctly when there are two names", () => {
+      const names = service.possessiveFormat(
+        `${cats[2].name} and ${cats[3].name}`
+      );
+      expect(names).toEqual(`${cats[2].name} and ${cats[3].name}'s`);
+    });
+  });
+});
---
+++ src/app.template-values.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class TemplateValuesService {
+  getSentenceFormattedNames(names: string[]): string {
+    return names.reduce((acc, name, index) => {
+      if (index === 0) {
+        return name;
+      }
+      if (index === names.length - 1) {
+        return `${acc} and ${name}`;
+      }
+      return `${acc}, ${name}`;
+    }, "");
+  }
+
+  possessiveFormat(namesSentence: string): string {
+    return `${namesSentence}'s`;
+  }
+}
