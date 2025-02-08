8461ee499fc6e069806bdeb49f64a99a50163923
Chee Diep <c.diep@hotmail.com>
2025-02-08 12:56:12 +0000

    Implement comms controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { users } from "../data";
+import { CommsController } from "./app.comms.controller";
+import { AppController } from "./app.controller";
+import { PricingService } from "./app.pricing.service";
+import { AppService } from "./app.service";
+import { TemplateValuesService } from "./app.template-values.service";
+import { UserService } from "./app.user.service";
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CommsController],
+  providers: [
+    AppService,
+    {
+      provide: UserService,
+      useFactory: () => new UserService(users),
+    },
+    PricingService,
+    TemplateValuesService,
+  ],
 })
 export class AppModule {}
